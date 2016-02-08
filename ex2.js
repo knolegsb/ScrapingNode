b8b6372f00642f98d20410f2c013099b30d6705d
knolegsb <seon2you@hotmail.com>
2016-02-07 20:15:11 -0800

    request, cheerio

---
+++ ex2.js
@@ -0,0 +1,42 @@
+﻿var express = require('express');
+var path = require('path');
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+var app = express();
+var port = 8000;
+
+var url = "http://www.indeed.com/jobs?q=.net&l=Seattle%2C+WA";
+request(url, function (err, resp, body) {
+    var $ = cheerio.load(body);
+    var companyName = $('.company');
+    var companyNameText = companyName.text();
+
+    //$('.company').filter(function () {
+    //    var companyName = $(this);
+    //    companyNameText = companyName.text();
+    //})
+    //console.log(companyNameText);
+
+    var jobTitle = $('.jobtitle');
+    var jobTitleText = jobTitle.text();
+    //console.log(jobTitleText);
+    var location = $('.location');
+    var locationText = location.text();
+
+    var summary = $('.summary');
+    var summaryText = summary.text();
+    //console.log(summaryText);
+    var job = {
+        jobTitle : jobTitleText,
+        location : locationText,
+        companyName : companyNameText,
+        summary : summaryText
+    };
+
+    console.log(job);
+});
+
+app.listen(port);
+console.log('Server is listening on ' + port);
---
+++ pinterest.js
@@ -0,0 +1,30 @@
+﻿var express = require('express');
+var path = require('path');
+var app = express();
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var port = 8080;
+
+var URL = "https://www.pinterest.com/pin/16395986122172500/";
+request(URL, function (err, resp, body) {
+    
+    var pin = {};
+    var $ = cheerio.load(body);
+
+    var img = $("meta[itemprop = 'image']").get(1);
+    var $img = $(img).attr('content');
+    var $desc = $("meta[itemprop = 'text']").attr('content');
+
+    var pin = {
+        img: $img,
+        desc: $desc,
+        url: URL
+    }
+
+    console.log("scraped: ", pin);
+});
+
+app.listen(port, function () {
+    console.log('Running server on port' + port);
+});
